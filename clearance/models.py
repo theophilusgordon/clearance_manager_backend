9ae801a66cf2fd1c41aea43bd50f661fd98ae67a
Theophilus Gordon <gordonfiifi@gmail.com>
2024-08-21 10:29:36 +0000

    refactor: add id field to admin

--- clearance/models.py
+++ clearance/models.py
@@ -23,6 +23,7 @@ class AdminManager(BaseUserManager):
         return self.create_user(username, email, password, **extra_fields)
 
 class Admin(AbstractBaseUser):
+    id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=150, unique=True)
     full_name = models.CharField(max_length=255)
     admin_number = models.CharField(max_length=20, unique=True)
